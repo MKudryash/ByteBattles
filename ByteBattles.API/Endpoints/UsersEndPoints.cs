02749083e5a420332f88f158eeb1ffb9b23c1558
MKudryash <82795441+MKudryash@users.noreply.github.com>
2024-04-04 23:23:41 +0300

    Preparation of the final registration and authorization methods

---
+++ ByteBattles.API/Endpoints/UsersEndPoints.cs
@@ -0,0 +1,24 @@
+﻿
+namespace ByteBattles.API.Endpoints
+{
+    public static class UsersEndPoints
+    {
+        public static IEndpointRouteBuilder MapUsersEndpoints(this IEndpointRouteBuilder app)
+        {
+            app.MapPost("SignUp", SigUp);
+            app.MapPost("SignIn", SigIn);
+
+            return app;
+        }
+
+        private static async Task<IResult> SigIn()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static async Task<IResult> SigUp()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
