b4cbe9ce18634cdb45a17ca6a6b292f3fa1fe299
MKudryash <82795441+MKudryash@users.noreply.github.com>
2024-04-04 17:42:45 +0300

    +Add Secret - change ByteBattlesDbContext

---
+++ ByteBattles.DataAccess/ByteBattlesDbContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using ByteBattles.DataAccess.Entites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ByteBattles.DataAccess;
+
+public partial class ByteBattlesDbContext : DbContext
+{
+
+    public ByteBattlesDbContext(DbContextOptions<ByteBattlesDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+    private readonly IConfiguration _configuration;
+    public ByteBattlesDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+ => optionsBuilder.UseNpgsql(_configuration.GetConnectionString("ConnectionStrings:ByteBattlesDB"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("users_pkey");
+
+            entity.ToTable("users");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.Enail)
+                .HasColumnType("character varying")
+                .HasColumnName("enail");
+            entity.Property(e => e.Encryptedpassword)
+                .HasColumnType("character varying")
+                .HasColumnName("encryptedpassword");
+            entity.Property(e => e.Username)
+                .HasMaxLength(25)
+                .HasColumnName("username");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ ByteBattles.DataAccess/Entites/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ByteBattles.DataAccess.Entites;
+
+public partial class User
+{
+    public Guid Id { get; set; }
+
+    public string? Username { get; set; }
+
+    public string? Encryptedpassword { get; set; }
+
+    public string? Enail { get; set; }
+}
