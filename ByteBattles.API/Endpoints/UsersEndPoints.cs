c51e7dcf85890109271feca20826f72a17cf86f9
MKudryash <82795441+MKudryash@users.noreply.github.com>
2024-04-05 17:44:34 +0300

    Add swagger autorize

--- ByteBattles.API/Endpoints/UsersEndPoints.cs
+++ ByteBattles.API/Endpoints/UsersEndPoints.cs
@@ -1,28 +1,26 @@
 ﻿
 using ByteBattles.API.Contracts.Users;
 using ByteBattles.Application.Services;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ByteBattles.API.Endpoints
 {
     public static class UsersEndPoints
     {
-        [Authorize]
         public static IEndpointRouteBuilder MapUsersEndpoints(this IEndpointRouteBuilder app)
         {
-             app.MapPost("api/SignUp", SigUp);
+             app.MapPost("api/SignUp", SigUp).RequireAuthorization();
             app.MapPost("api/SignIn", SigIn);
 
             return app;
         }
-
         private static async Task<IResult> SigUp([FromBody] SignUpRequest signUpRequest, UsersServices usersServices)
         {
             await usersServices.SignUp(signUpRequest.UserName,signUpRequest.Email,signUpRequest.Password);
 
             return Results.Ok();
         }
+       
 
         private static async Task<IResult> SigIn(SignInRequest request,UsersServices usersServices, HttpContext context)
         {
